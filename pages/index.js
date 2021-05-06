2aa25704c46b837496c5fbcd5e656fd930c5d646
kobpeapoo <kobpeapoo0425@gmail.com>
2021-05-06 23:16:43 +0700

    add thai

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          ยินดีต้อนรับทุกท่านเข้าสู่ Next JS Workshop
         </h1>
 
 
